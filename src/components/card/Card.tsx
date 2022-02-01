ebaf08d69d089c6807314416dcc5dcad7d60c006
sandvik16 <sannndvik16@gmail.com>
2022-02-01 17:24:19 +0300

    feat: change name new task

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import styled from "styled-components";
 import {Description} from "./description/Description";
 import {Comments} from "./comments/Comments";
@@ -20,6 +20,9 @@ const ColumnName = styled.span`
 const Name = styled.span`
   border: 1px solid black;
 `
+const IputText = styled.input`
+    
+`
 
 type PropsType = {
     state: DataT,
@@ -35,18 +38,44 @@ type PropsType = {
 
 export const Card = (props: PropsType) => {
 
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+    const [cardName, setCardName] = useState(props.cardName)
+
 
     const clickHandler = () => {
         setClick(!click)
     }
 
+    const changeInput = () => {
+        setIsEdit(true)
+    }
+
+    const changeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setCardName(e.currentTarget.value)
+    }
+
+    const blurHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setIsEdit(false)
+        let findColumn = props.state.columns.find(item => item.id === props.columnId);
+        //@ts-ignore
+        let findCard = findColumn.cards.find(item => item.id === props.cardId);
+        //@ts-ignore
+        findCard.title = e.currentTarget.value
+        let newColumns = props.state.columns.map(item => item.id === props.columnId ? findColumn : item)
+        let newState = {columns: newColumns}
+        props.setState(newState)
+    }
+
     return (
         <>
             <Name onClick={clickHandler}>{props.cardName}</Name>
+
             {click ? <CardWrapper>
-                <ColumnName>в колонке {props.columnName}</ColumnName>
-                <CardName>{props.cardName}</CardName>
+                   <ColumnName>в колонке {props.columnName}</ColumnName>
+
+                {!isEdit ? <CardName onDoubleClick={changeInput}>{props.cardName}</CardName>
+                    : <IputText type='text' value={cardName} onChange={changeHandler} onBlur={blurHandler}/>}
 
                 <Description state={props.state}
                              cardId={props.cardId}
